2c36c2b5ac3f9b6eed4bc731e5f2eb718db07b84
hyualy <kko20_s23_20806@gclass.ice.go.kr>
2024-05-21 16:50:29 +0900

    Alpha 0.3.5

---
+++ app/src/main/java/com/hyualy/mdp/activity/ControlActivity.kt
@@ -0,0 +1,12 @@
+package com.hyualy.mdp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hyualy.mdp.R
+
+class ControlActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control)
+    }
+}
