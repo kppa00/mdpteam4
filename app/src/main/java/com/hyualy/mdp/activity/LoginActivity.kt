7671d5318c86fe2d859d177e71bc89c079430171
hyualy <kko20_s23_20806@gclass.ice.go.kr>
2024-05-21 16:04:09 +0900

    Alpha 0.3.4

--- app/src/main/java/com/hyualy/mdp/activity/LoginActivity.kt
+++ app/src/main/java/com/hyualy/mdp/activity/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.hyualy.mdp.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.hyualy.mdp.R
 
@@ -8,5 +11,20 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        settingButtons()
+    }
+
+    private fun settingButtons() {
+        val btnRegister = findViewById<TextView>(R.id.login_register)
+        btnRegister.setOnClickListener {
+            val intentRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegister)
+            finish()
+        }
+        val btnLogin = findViewById<Button>(R.id.login_login)
+        btnLogin.setOnClickListener {
+            TODO("Login")
+        }
     }
 }
--- app/src/main/java/com/hyualy/mdp/activity/RegisterActivity.kt
+++ app/src/main/java/com/hyualy/mdp/activity/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.hyualy.mdp.activity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.hyualy.mdp.R
 
@@ -8,5 +9,13 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        settingButton()
+    }
+
+    private fun settingButton() {
+        val btnRegister = findViewById<Button>(R.id.register_register)
+        btnRegister.setOnClickListener {
+            TODO("register")
+        }
     }
 }
