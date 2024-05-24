27cc15c749b7070a3fa0d91a4d841641ff05c81f
hyualy <kko20_s23_20806@gclass.ice.go.kr>
2024-05-24 15:12:54 +0900

    Alpha 0.3.6

--- app/src/main/java/com/hyualy/mdp/activity/LobbyActivity.kt
+++ app/src/main/java/com/hyualy/mdp/activity/LobbyActivity.kt
@@ -1,10 +1,17 @@
 package com.hyualy.mdp.activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.hyualy.mdp.R
+import com.hyualy.mdp.fragment.AccountFragment
+import com.hyualy.mdp.fragment.StartFragment
 import com.hyualy.mdp.util.Util
 
 class LobbyActivity : AppCompatActivity() {
@@ -12,21 +19,53 @@ class LobbyActivity : AppCompatActivity() {
         setTheme(R.style.Theme_MDP)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.lobby_fragment, StartFragment())
+                .commit()
+        }
 
         Util.twoBackToFinish(this)
-        settingButtons()
     }
 
-    private fun settingButtons() {
-        val btnLogin = findViewById<Button>(R.id.lobby_login)
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        startSettingButtons()
+        return super.onCreateView(name, context, attrs)
+    }
+
+    private fun startSettingButtons() {
+        val btnRegister = findViewById<TextView>(R.id.lobby_account_start)
+        btnRegister.setOnClickListener {
+            val intentRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegister)
+            finish()
+        }
+//        replaceFragment(PermissionFragment())
+//        permissionSettingButtons()
+    }
+
+    private fun permissionSettingButtons() {
+        replaceFragment(AccountFragment())
+        accountSettingButtons()
+    }
+
+    private fun accountSettingButtons() {
+        val btnLogin = findViewById<Button>(R.id.lobby_account_login)
         btnLogin.setOnClickListener {
             val intentLogin = Intent(this, LoginActivity::class.java)
             startActivity(intentLogin)
         }
-        val btnRegister = findViewById<Button>(R.id.lobby_register)
+        val btnRegister = findViewById<Button>(R.id.lobby_account_register)
         btnRegister.setOnClickListener {
             val intentRegister = Intent(this, RegisterActivity::class.java)
             startActivity(intentRegister)
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.lobby_fragment, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/hyualy/mdp/fragment/AccountFragment.kt
@@ -0,0 +1,17 @@
+package com.hyualy.mdp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hyualy.mdp.R
+
+class AccountFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
