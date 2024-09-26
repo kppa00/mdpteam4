46a1b88503d55b174d089d1fdedf9a52a51e5082
hyualy <136699281+hyualy@users.noreply.github.com>
2024-09-26 23:30:21 +0900

    Update AF_main.py

--- Raspberry Pi 5 codes/AF_main.py
+++ Raspberry Pi 5 codes/AF_main.py
@@ -1,82 +1,78 @@
-import cv2
-from ultralytics import YOLO
-import AF_serial
+import threading
 import AF_database
-import time
+import AF_wifi
+import AF_serial
 
-model = YOLO('best.pt')
+isRunning = False
 
-def is_opened_camera():
-    for i in range(0, 11):
-        cap = cv2.VideoCapture(i)
+def login(msg, ip):
+    sent_data = msg.split('/')
+    user_info = AF_database.query(f"SELECT * FROM account WHERE id = '{sent_data[1]}' AND password = '{sent_data[2]}'")
+    if len(user_info) == 1:
+        print("Login Successful\n")
+        AF_wifi.send_data(f"login/yes/{user_info[0][3]}", ip)
+    else:
+        print("Login failed\n")
+        AF_wifi.send_data("login/no", ip)
 
-        if not cap.isOpened():
-            print(f"can't open camera{i}")
-        else:
-            print(f"opened camera {i}")
+def register(msg, ip):
+    sent_data = msg.split('/')
+    user_info = AF_database.query(f"SELECT * FROM account WHERE id = '{sent_data[1]}'")
 
-def start_camera():
-    caps = []
-    cap_count = 0
-    is_defective = False
-    last_bottle_time = 0
+    idSame = False
+    if len(user_info) != 0:
+        for data in user_info:
+            if sent_data[1] == data[1]:
+                idSame = True
+    if idSame:
+        print('Register failed\n')
+        AF_wifi.send_data('register/no', ip)
+    else:
+        print('Register successful\n')
+        AF_database.query(f"INSERT INTO account VALUE (NULL, '{sent_data[1]}', '{sent_data[2]}', '{sent_data[3]}', '{sent_data[4]}')")
+        AF_wifi.send_data('register/yes', ip)
 
-    for i in range(0, 11):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            cap_count += 1
-            caps.append(cap)
-            print(f"opened camera {i}")
-        if cap_count == 2:
-            break
+def control(msg, ip):
+    global isRunning
+    sent_data = msg.split('/')
+    if sent_data[1] == "switching" or "isRunning":
+        if sent_data[1] == "isRunning":
+            if isRunning:
+                AF_wifi.send_data("control/yes", ip)
+            else:
+                AF_wifi.send_data("control/no", ip)
+        if sent_data[1] == "switching":
+            isRunning = not isRunning
+            if isRunning:
+                AF_serial.send_data("belton")
+            else:
+                AF_serial.send_data("beltof")
+        print("Now running is :", isRunning, "\n")
 
-    for cap_num, cap in enumerate(caps):
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+def bottle(msg, ip):
+    sent_data = msg.split('/')
+    if sent_data[1] == "getCount":
+        bottle_count = AF_database.query("SELECT * FROM bottle")
+        print("good / defect :", str(bottle_count[0][0]) + " / " + str(bottle_count[0][1]) + "\n")
+        AF_wifi.send_data(str(bottle_count[0][0]) + "/" + str(bottle_count[0][1]), ip)
 
-    while True:
-        for cap_num, cap in enumerate(caps):
-            ret, frame = cap.read()
-            if not ret:
-                print(f"can't read frame camera{cap_num}")
-                break
+def belt():
+    isRunning = not isRunning
+    AF_wifi.send_data("control/belt")
 
-            results = model(frame)
+def defect_checker():
+    print()
+    # 이미지 인식
+    # 전처리
+    # 추론
+    # DB에 결과 저장
+    # AF_serial.send_data("defect")
 
-            for result in results:
-                boxes = result.boxes
-                for box in boxes:
-                    confidence = box.conf[0]
-                    if confidence > 0.5:
-                        result_class = model.names[int(box.cls[0])]
+def main():
+    receive_thread = threading.Thread(target=AF_wifi.receive_data)
+    receive_thread.start()
 
-                        if result_class == "bottle":
-                            current_time = time.time()
-                            if current_time - last_bottle_time >= 1:
-                                if not is_defective:
-                                    AF_database.query("update 테이블명 set 컬럼명 = 컬럼명 +1")
-                                is_defective = False
-                            last_bottle_time = current_time
+    # AF_serial.send_data("defect")
 
-                        if (result_class == "defect" or result == "empty") and not is_defective:
-                            is_defective = True
-                            AF_serial.send_data("defect")
-                            AF_database.query("update 테이블명 set 컬럼명 = 컬럼명 +1")
-
-                        x1, y1, x2, y2 = map(int, box.xyxy[0])
-
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
-                        label = f"{result_class} {confidence:.2f}"
-                        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-            cv2.imshow(f'camera{cap_num}', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    for cap in caps:
-        cap.release()
-    cv2.destroyAllWindows()
-
-start_camera()
+if __name__ == "__main__":
+    main()
